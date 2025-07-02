c8fa73a27adf4ec1205365856408520e1b236540
Árthur Tolfo Pinheiro <arthur.pinheiro@elsys.com.br>
2025-07-02 12:12:41 -0300

    Add project files.

---
+++ 3. Fundamentals/tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace Customers.Api.Tests.Integration;
+
+public class CustomerControllerTests
+{
+    [Fact]
+    public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExist()
+    {
+        // Arrange
+        var customerId = Guid.NewGuid();
+        
+        // Act
+        var response = await new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:5001")
+        }.GetAsync($"customers/{customerId}");
+
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
